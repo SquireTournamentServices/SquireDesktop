283e077a4a21598706b245436cf7c1a0343ad642
Danny Piper <djpiper28@gmail.com>
2022-04-27 17:29:23 +0100

    feat: Init qt project

---
+++ SquireDesktop_en_GB.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_GB">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <location filename="SquireDesktop/mainwindow.ui" line="14"/>
+        <location filename="build/SquireDesktop_autogen/include/ui_mainwindow.h" line="50"/>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
