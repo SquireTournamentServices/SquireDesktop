aa4ffdf1b751ad1ab9a52cf4a843f1b1c8599d81
Danny Piper <djpiper28@gmail.com>
2022-04-28 21:39:04 +0100

    feat: link rust to c++

--- SquireDesktop_en_GB.ts
+++ SquireDesktop_en_GB.ts
@@ -6,6 +6,7 @@
     <message>
         <location filename="src/ui/mainwindow.ui" line="14"/>
         <location filename="build/SquireDesktop_autogen/include/ui_mainwindow.h" line="50"/>
+        <location filename="build-SquireDesktop-Desktop-Debug/SquireDesktop_autogen/include/ui_mainwindow.h" line="50"/>
         <source>MainWindow</source>
         <translation type="unfinished"></translation>
     </message>
