b8484723549c8ef4f85c111e90387ad59d2713e1
Danny Piper <djpiper28@gmail.com>
2022-05-02 19:20:03 +0100

    feat: added Squire Core linker and, linker tests.

---
+++ tests/sq_link_test.cpp
@@ -0,0 +1,8 @@
+#include "squire_core/squire_core.h"
+#include "./sq_link_test.h"
+
+void test_linker()
+{
+  squire_core::sc_Tournament *t;
+}
+
---
+++ tests/sq_link_test.h
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace squire_core {
+  class sc_Tournament; // Forward declare for the tests
+};
+void test_linker();
+
