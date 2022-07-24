158d2af5b1ae8e94142817c12a542ac85e11d37f
Danny Piper <djpiper28@gmail.com>
2022-07-24 17:37:52 +0100

    fix: version number can be selected (and copied) for reporting issues

--- SquireDesktop_en_GB.ts
+++ SquireDesktop_en_GB.ts
@@ -305,13 +305,13 @@
     <name>MainWindow</name>
     <message>
         <location filename="src/ui/mainwindow.ui" line="14"/>
-        <location filename="build/SquireDesktop_autogen/include/ui_mainwindow.h" line="79"/>
+        <location filename="build/SquireDesktop_autogen/include/ui_mainwindow.h" line="80"/>
         <source>MainWindow</source>
         <translation></translation>
     </message>
     <message>
         <location filename="src/ui/mainwindow.ui" line="24"/>
-        <location filename="build/SquireDesktop_autogen/include/ui_mainwindow.h" line="80"/>
+        <location filename="build/SquireDesktop_autogen/include/ui_mainwindow.h" line="81"/>
         <source>Version info, replaced at startup - don&apos;t translate</source>
         <translation></translation>
     </message>
