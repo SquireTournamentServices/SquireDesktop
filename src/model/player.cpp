e429128a9143daabc14975bca47488775fca9980
Danny Piper <djpiper28@gmail.com>
2022-08-05 17:04:37 +0100

    feat: added player class with basic getters

---
+++ src/model/player.cpp
@@ -0,0 +1,49 @@
+#include "./player.h"
+#include <string.h>
+
+Player::Player(squire_core::sc_PlayerId pid, squire_core::sc_TournamentId tid)
+{
+    this->pid = pid;
+    this->tid = tid;
+}
+
+Player::~Player()
+{
+
+}
+
+std::string Player::name()
+{
+    char *name = (char *)squire_core::pid_name(this->pid, this->tid);
+    std::string ret = std::string(name);
+    free(name);
+    return ret;
+}
+
+std::string Player::gameName()
+{
+    char *name = (char *)squire_core::pid_game_name(this->pid, this->tid);
+    std::string ret = std::string(name);
+    free(name);
+    return ret;
+
+}
+
+squire_core::sc_PlayerStatus Player::status()
+{
+    return squire_core::pid_status(this->pid, this->tid);
+}
+
+squire_core::sc_PlayerId Player::id()
+{
+    squire_core::sc_PlayerId ret;
+    memcpy(ret._0, this->pid._0, sizeof(ret));
+    return ret;
+}
+
+squire_core::sc_TournamentId Player::tourn_id()
+{
+    squire_core::sc_TournamentId ret;
+    memcpy(ret._0, this->tid._0, sizeof(ret));
+    return ret;
+}
---
+++ src/model/player.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <string>
+#include <squire_core/squire_core.h>
+
+class Player
+{
+public:
+    Player(squire_core::sc_PlayerId pid, squire_core::sc_TournamentId tid);
+    ~Player();
+    std::string name();
+    std::string gameName();
+    squire_core::sc_PlayerStatus status();
+    squire_core::sc_PlayerId id();
+    squire_core::sc_TournamentId tourn_id();
+private:
+    squire_core::sc_PlayerId pid;
+    squire_core::sc_TournamentId tid;
+};
