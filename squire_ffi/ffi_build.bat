8dc75735194c08954c8fc031eeea6f1309c14b05
TylerBloom <tylerbloom2222@gmail.com>
2023-01-22 12:44:37 -0500

    Removed squire_core as submodule and added an ffi library

---
+++ squire_ffi/ffi_build.bat
@@ -0,0 +1,8 @@
+python bindgen.py
+cbindgen --config cbindgen.toml --crate squire_lib --output squire_core.h -v
+echo "Exported to ./squire_core.h"
+cargo build --features ffi --package squire_lib --release
+
+echo "Removing polymorphism"
+python postprocess.py
+
